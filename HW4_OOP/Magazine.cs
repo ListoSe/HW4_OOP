d95bb6e2586398d987bcb57eb66ec679e5dba6c5
Sergii Chernyshov <atiska200612@gmail.com>
2023-02-23 14:09:31 +0200

    Добавьте файлы проекта.

---
+++ HW4_OOP/Magazine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_OOP
+{
+    internal class Magazine
+    {
+        private string name;
+        private string description;
+        private string phone;
+        private string email;
+        public Magazine() { }
+        public Magazine(string name, string description, string phone, string email) { 
+            this.name = name;
+            this.description = description;
+            this.phone = phone;
+            this.email = email;
+        }
+
+        public void SetName(string name) { this.name = name; }
+        public void SetDescription(string description) { this.description = description; }
+        public void SetPhone(string phone) { this.phone = phone; }
+        public void SetEmail(string email) { this.email = email; }
+
+        public string GetName() { return name; }
+        public string GetDescription() { return description; }
+        public string GetPhone() { return phone; }
+        public string GetEmail() { return email; }
+
+    }
+}
---
+++ HW4_OOP/WedSite.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_OOP
+{
+    internal class WedSite
+    {
+        private string name;
+        private string url;
+        private string description;
+        private string ip;
+
+        public WedSite(){}
+        public WedSite(string name, string url, string description, string ip) {
+            this.name = name;  
+            this.url = url;
+            this.description = description;
+            this.ip = ip;
+        }
+
+        public void SetName(string name) { this.name = name; }
+        public void SetUrl(string url) { this.url = url; }
+        public void SetDescription(string description) { this.description = description; }
+        public void SetIp(string ip) { this.ip = ip; }
+
+        public string GetName() { return name; }
+        public string GetUrl() { return url; }
+        public string GetDescription() { return description; }
+        public string GetIp() { return ip; }
+
+    }
+}
